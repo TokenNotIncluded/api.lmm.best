1347bc51c65e98c889823986a6813a1384c96bad
LIghtJUNction <lightjunction.me@gmail.com>
2026-09-14 05:01:19 +0800

    fix: preserve deployment package ownership

--- scripts/auto-deploy-production-release.sh
+++ scripts/auto-deploy-production-release.sh
@@ -115,14 +115,17 @@ for rollback_component in go web; do
     bash /repo/scripts/build-release-package.sh "$rollback_component" "$rollback_version" \
       "/work/rollback/$rollback_package" "/work/rollback/$rollback_package.pkg.tar.zst" \
       /repo "$rollback_pkgrel"
+  chown "$(id -u):$(id -g)" "$root/rollback/$rollback_package.pkg.tar.zst"
 done
 
 if [[ "$component" == go ]]; then
   docker run --rm --network host -v "$GITHUB_WORKSPACE:/repo:ro" -v "$root:/work" archlinux:base-devel \
     bash /repo/scripts/build-release-package.sh go "$version" /work/assets /work/pkg/lmm-api-go-bin.pkg.tar.zst /repo
+  chown "$(id -u):$(id -g)" "$root/pkg/lmm-api-go-bin.pkg.tar.zst"
 else
   docker run --rm --network host -v "$GITHUB_WORKSPACE:/repo:ro" -v "$root:/work" archlinux:base-devel \
     bash /repo/scripts/build-release-package.sh web "$version" /work/assets /work/pkg/lmm-api-web-bin.pkg.tar.zst /repo
+  chown "$(id -u):$(id -g)" "$root/pkg/lmm-api-web-bin.pkg.tar.zst"
 fi
 
 go_candidate="$root/rollback/lmm-api-go-bin.pkg.tar.zst"; web_candidate="$root/rollback/lmm-api-web-bin.pkg.tar.zst"
