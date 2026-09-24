f27b018d805957c3df3d139fdf995eb5416b07f2
LIghtJUNction <lightjunction.me@gmail.com>
2026-09-24 14:34:34 +0800

    fix(api): add missing-group token message

--- apps/api-go/i18n/keys.go
+++ apps/api-go/i18n/keys.go
@@ -43,6 +43,7 @@ const (
 // Token related messages
 const (
 	MsgTokenNameTooLong          = "token.name_too_long"
+	MsgTokenGroupRequired        = "token.group_required"
 	MsgTokenQuotaNegative        = "token.quota_negative"
 	MsgTokenQuotaExceedMax       = "token.quota_exceed_max"
 	MsgTokenGenerateFailed       = "token.generate_failed"
